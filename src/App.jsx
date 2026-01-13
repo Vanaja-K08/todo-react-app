136389e40b8423ad16d39c21513c023372cd6d18
Vanaja-K08 <vanajakoppad@gmail.com>
2026-01-13 13:17:53 +0530

    implemented text field and validation , event handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,54 @@
+import { useState } from 'react';
 import './App.css'
 import Header from './component/Header'
 import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap
+import ToDoList from './component/ToDoList';
 
 function App() {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [error, setError] = useState("");
 
+  const addTodo = () => {
+    if (task.trim() === "") {
+      setError("Task cannot be empty!");
+      return;
+    }
+    setTodos([
+      ...todos,
+      {
+        id: Date.now(),
+        text: task,
+        completed: false,
+      },
+    ]);
+
+    setTask("");
+    setError("");
+  };
   return (
-    <>
-     <Header/>
-    </>
-  )
+    <div className="container mt-5 bg-primar">
+      <Header />
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter a task..."
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button className="btn btn-primary" onClick={addTodo}>
+          Add
+        </button>
+      </div>
+
+      {error && <div className="alert alert-danger">{error}</div>}
+
+      <ToDoList
+        todos={todos}
+      />
+    </div>
+  );
 }
 
 export default App
---
+++ src/component/ToDoList.jsx
@@ -0,0 +1,11 @@
+function ToDoList({todos}){
+
+    if(todos.length === 0){
+        return(
+            <p className="text-center">No task added</p>
+        )
+    }
+
+}
+
+export default ToDoList;
